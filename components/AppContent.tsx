b7e96fea5bbd32811c2336effd14f9d9f449cf7b
justxd22 <xdjust18@gmail.com>
2026-01-04 01:26:19 +0200

    Fix safari

--- components/AppContent.tsx
+++ components/AppContent.tsx
@@ -984,6 +984,10 @@ function MainContent() {
               bgm.currentTime = 0; // Reset
               bgm.volume = 0;
               console.log("BGM Ready to Play on Act 6", bgm.currentTime, bgm.volume);
+              bgm.currentTime = 0;
+              const fade = setInterval(() => (bgm.volume = Math.max(bgm.volume - 0.05, 0)) || (bgm.volume === 0 && clearInterval(fade)), 50);
+              console.log("BGM Ready to Play on Act 6", bgm.currentTime, bgm.volume);
+
           }).catch(e => console.error("BGM Unlock Failed", e));
       }
   };
